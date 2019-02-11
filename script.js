0c8fb966c32bc2e380e844891553a455a7efa384
Michal <mszyszka223@gmail.com>
2019-02-11 11:27:36 +0100

    header for small dev done. Working on section.co-mozemy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('.hamburger').on('click', function(){
     $('.hamburger').toggleClass('change');
     $('.nav-ul').toggleClass('open');
-    $('.header-nav').toggleClass('nav-open');
+    $('.top-nav').toggleClass('nav-open');
 });
 
